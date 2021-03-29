7261e6bb18d2ff0303295a377b712ee662ce0dbc
Mykola Bubelich <thesimj@users.noreply.github.com>
2021-03-29 22:10:27 +0200

    update container parameters

--- src/models.py
+++ src/models.py
@@ -7,6 +7,12 @@ class ContainerParameters(BaseModel):
     name: str
     image: str
     environment: List[str] = []
+    network_mode: Optional[str]
+    cpu_period: Optional[int]
+    cpu_quota: Optional[int]
+    devices: Optional[List[str]]
+    tmpfs: Optional[Dict]
+    volumes: Optional[Dict]
 
 
 class Container(BaseModel):

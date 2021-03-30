ee04621f5f3fa200f2e945bbd49b47fef0c14f38
Mykola Bubelich <thesimj@users.noreply.github.com>
2021-03-30 10:43:59 +0200

    add catch exception when url not available

--- src/helpers.py
+++ src/helpers.py
@@ -8,6 +8,10 @@ from src.models import Container
 def http_get_containers(url: str, key: str, agent: str = "kapellmeister-agent") -> Optional[List[Container]]:
     headers: Dict = {"Authorization": f"Token {key}", "User-Agent": agent, "Accept-Encoding": "gzip"}
 
-    with requests.get(url, headers=headers) as r:
-        if r.ok:
-            return [Container.parse_obj(c) for c in r.json()]
+    try:
+        with requests.get(url, headers=headers, timeout=30) as r:
+            if r.ok:
+                return [Container.parse_obj(c) for c in r.json()]
+
+    except requests.RequestException as err:
+        print(err)
